32932f76d5baa6cebc1146f9905ed93cf709f114
HeyLeidl <113834398+HeyLeidl@users.noreply.github.com>
2023-03-01 21:26:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Izudin Muheljic
+//1 AHIF 
+// Find my rovusing System;
+
+double westcounter = 0;
+double eastcounter = 0;
+double southcounter = 0;
+double northcounter = 0;
+WelcomeMessage();
+void WelcomeMessage()
+{
+    Console.WriteLine("Welcome to FindMyRover");
+    Console.WriteLine("######################");
+    Console.WriteLine("");
+}
+Console.WriteLine("Enter the movement log of your rover!");
+string movementlog = Console.ReadLine()!;
+
+eastcounter = movementlog.Count(c=>c=='>');
+westcounter = movementlog.Count(c=>c=='<');
+northcounter = movementlog.Count(c=>c=='^');
+southcounter = movementlog.Count(c=>c=='V');
+
+
+double y = northcounter - southcounter;
+double x = eastcounter - westcounter;
+Console.WriteLine("X =" + x );
+Console.WriteLine("Y =" + y);    
+Console.WriteLine($"The rover is {eastcounter}m to the east");
+Console.WriteLine($"The rover is {westcounter}m to the west");
+Console.WriteLine($"The rover is {northcounter}m to the north");
+Console.WriteLine($"The rover is {southcounter}m to the south");
